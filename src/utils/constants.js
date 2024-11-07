4dff1e4e498794af04e620ba0a2164c31bae7c3e
Jason <jason25840@gmail.com>
2024-11-07 15:18:39 -0500

    hide apikey

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,5 @@
-export const APIkey = "***REMOVED***";
-export const BASE_URL = process.env.NODE_ENV === "production" 
-? "https://api.newsexplorer.hackquest.com"
-: "http://localhost:3001";
+export const APIkey = process.env.REACT_APP_API_KEY;
+export const BASE_URL =
+  process.env.NODE_ENV === "production"
+    ? "https://api.newsexplorer.hackquest.com"
+    : "http://localhost:3001";
